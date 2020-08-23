ad76d4bc9830b2e1e497f4c3069e6ef8c6748022
Brian Dawn <brian-dawn@users.noreply.github.com>
2020-08-22 21:53:01 -0500

    Brian/refactor (#2)
    * Break apart lib.rs into multiple files.
    
    * Run cargo fix.
    
    * Cleanup compiler warnings.

---
+++ src/number.rs
@@ -0,0 +1,99 @@
+
+
+
+
+
+use std::fmt;
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+use crate::utils::gcd;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum Number {
+    Fractional(i64, i64),
+    Rounded(f64),
+}
+
+impl Number {
+    fn reduce(self) -> Self {
+        match self {
+            Number::Fractional(n, d) => {
+                let gcd = gcd(n, d);
+                Number::Fractional(n / gcd, d / gcd)
+            }
+            Number::Rounded(_) => self,
+        }
+    }
+}
+
+impl From<Number> for f64 {
+    fn from(item: Number) -> Self {
+        match item {
+            Number::Fractional(n, d) => n as f64 / d as f64,
+            Number::Rounded(v) => v,
+        }
+    }
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let display = self.reduce();
+        match display {
+            Number::Fractional(n, 1) => write!(f, "{}", n),
+            Number::Fractional(n, d) => write!(f, "{}/{}", n, d),
+            Number::Rounded(num) => write!(f, "{}", num),
+        }
+    }
+}
+
+impl Add for Number {
+    type Output = Number;
+    fn add(self, rhs: Self) -> Self::Output {
+        if let Number::Fractional(n, d) = self {
+            if let Number::Fractional(rn, rd) = rhs {
+                return Number::Fractional(n * rd + rn * d, rd * d);
+            }
+        }
+        Number::Rounded(f64::from(self) + f64::from(rhs))
+    }
+}
+
+impl Neg for Number {
+    type Output = Number;
+    fn neg(self) -> Self::Output {
+        match self {
+            Number::Fractional(n, d) => Number::Fractional(-n, d),
+            Number::Rounded(n) => Number::Rounded(-n),
+        }
+    }
+}
+
+impl Sub for Number {
+    type Output = Number;
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + (-rhs)
+    }
+}
+
+impl Mul for Number {
+    type Output = Number;
+    fn mul(self, rhs: Self) -> Self::Output {
+        if let Number::Fractional(a, b) = self {
+            if let Number::Fractional(c, d) = rhs {
+                return Number::Fractional(a * c, b * d);
+            }
+        }
+
+        Number::Rounded(f64::from(self) * f64::from(rhs))
+    }
+}
+
+impl Div for Number {
+    type Output = Number;
+    fn div(self, rhs: Self) -> Self::Output {
+        match rhs {
+            Number::Fractional(n, d) => self * Number::Fractional(d, n),
+            Number::Rounded(n) => Number::Rounded(f64::from(self) / n),
+        }
+    }
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -2,10 +2,13 @@ use trusty_calculator;
 
 #[cfg(test)]
 mod tests {
+    use super::trusty_calculator::number::*;
+    use super::trusty_calculator::parser::*;
     use super::trusty_calculator::*;
-    use anyhow::Result;
+
+    
     use assert_cmd::prelude::*;
-    use std::process::Command;
+    
 
     #[test]
     fn test_walk() {
@@ -29,122 +32,5 @@ mod tests {
 
         let n = walk(&parse("1 / 2 + 2 / 3").unwrap());
         assert_eq!(n, Number::Fractional(7, 6));
-    }
-
-    fn test_number_cmd() -> Result<()> {
-        let mut cmd = Command::cargo_bin("trusty-calculator")?;
-
-        cmd.arg("3");
-        cmd.assert().success().stdout("3\n");
-
-        Ok(())
-    }
-
-    fn test_add_cmd() -> Result<()> {
-        let mut cmd = Command::cargo_bin("trusty-calculator")?;
-
-        cmd.arg("1+1");
-        cmd.assert().success().stdout("2\n");
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_parse_number() {
-        assert_eq!(parse_integer("1"), Ok(("", 1)));
-        assert!(parse_float("1").is_err());
-        assert!(parse_integer("-1").is_err());
-        assert_eq!(parse_integer("1 + 2"), Ok((" + 2", 1)));
-        assert_eq!(parse_integer("12"), Ok(("", 12)));
-        assert_eq!(parse_float("1.2"), Ok(("", 1.2)));
-        assert!(parse_float("-1.2").is_err());
-        assert_eq!(parse_float("inf"), Ok(("", f64::INFINITY)));
-        assert!(parse_float("-inf").is_err());
-
-        assert_eq!(parse_number("1"), Ok(("", Number::Fractional(1, 1))));
-        assert!(parse_number("-1").is_err());
-        assert_eq!(parse_number("1.2"), Ok(("", Number::Rounded(1.2))));
-
-        // TODO
-        //assert_eq!(parse_float("∞"), Ok(("", f64::INFINITY)));
-    }
-
-    fn test_negate_parse_expr() {
-        // assert_eq!(parse_expr("-2"), Ok(("", Number::Fractional(-2))));
-        // assert_eq!(parse_expr("-20"), Ok(("", Number::Fractional(-20))));
-        // assert_eq!(parse_expr("-1.2"), Ok(("", Number::Fractional(-1.2))));
-    }
-
-    #[test]
-    fn test_parse_add_and_sub() {
-        let one = Number::Fractional(1, 1);
-        let two = Number::Fractional(2, 1);
-        let three = Number::Fractional(3, 1);
-
-        assert_eq!(
-            parse_add("1+3"),
-            Ok((
-                "",
-                Expr::Add(Box::new(Expr::Number(one)), Box::new(Expr::Number(three)))
-            ))
-        );
-
-        assert_eq!(
-            parse_sub("1-3"),
-            Ok((
-                "",
-                Expr::Sub(Box::new(Expr::Number(one)), Box::new(Expr::Number(three)))
-            ))
-        );
-
-        assert_eq!(
-            parse_add("1+2+3"),
-            Ok((
-                "",
-                Expr::Add(
-                    Box::new(Expr::Number(one)),
-                    Box::new(Expr::Add(
-                        Box::new(Expr::Number(two)),
-                        Box::new(Expr::Number(three))
-                    )),
-                )
-            ))
-        );
-    }
-    #[test]
-    fn test_parse_mul_and_div() {
-        let one = Number::Fractional(1, 1);
-        let two = Number::Fractional(2, 1);
-        let three = Number::Fractional(3, 1);
-
-        assert_eq!(
-            parse_mul("1*3"),
-            Ok((
-                "",
-                Expr::Mul(Box::new(Expr::Number(one)), Box::new(Expr::Number(three)))
-            ))
-        );
-
-        assert_eq!(
-            parse_div("1/3"),
-            Ok((
-                "",
-                Expr::Div(Box::new(Expr::Number(one)), Box::new(Expr::Number(three)))
-            ))
-        );
-
-        assert_eq!(
-            parse_mul("1*2*3"),
-            Ok((
-                "",
-                Expr::Mul(
-                    Box::new(Expr::Number(one)),
-                    Box::new(Expr::Mul(
-                        Box::new(Expr::Number(two)),
-                        Box::new(Expr::Number(three))
-                    )),
-                )
-            ))
-        );
     }
 }
