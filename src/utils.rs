012fd7465fc84846a3008a2784a2782d38ba88d9
Brian Dawn <brian.t.dawn@gmail.com>
2020-08-23 08:38:28 -0500

    Clean up decimal to rational to allow for edgecase handling.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,4 @@
-pub fn gcd(x: i64, y: i64) -> i64 {
-    let mut x = x;
-    let mut y = y;
+pub fn gcd(mut x: i64, mut y: i64) -> i64 {
     while y != 0 {
         let t = y;
         y = x % y;
@@ -12,7 +10,11 @@ pub fn gcd(x: i64, y: i64) -> i64 {
 /// Attempt to convert a decimal to a rational number.
 /// https://rosettacode.org/wiki/Convert_decimal_number_to_rational#Rust
 #[allow(clippy::many_single_char_names)]
-pub fn decimal_to_rational(mut n: f64) -> [i64; 2] {
+pub fn decimal_to_rational(mut n: f64) -> Option<(i64, i64)> {
+    if n.is_nan() || n.is_infinite() {
+        return None;
+    }
+
     //Based on Farey sequences
     assert!(n.is_finite());
     let flag_neg = n < 0.0;
@@ -20,10 +22,10 @@ pub fn decimal_to_rational(mut n: f64) -> [i64; 2] {
         n *= -1.0
     }
     if n < std::f64::MIN_POSITIVE {
-        return [0, 1];
+        return Some((0, 1));
     }
     if (n - n.round()).abs() < std::f64::EPSILON {
-        return [n.round() as i64, 1];
+        return Some((n.round() as i64, 1));
     }
     let mut a: i64 = 0;
     let mut b: i64 = 1;
@@ -46,8 +48,22 @@ pub fn decimal_to_rational(mut n: f64) -> [i64; 2] {
     // Make sure that the fraction is irreducible
     let gcd = gcd(a + c, b + d);
     if flag_neg {
-        [-(a + c) / gcd, (b + d) / gcd]
+        Some((-(a + c) / gcd, (b + d) / gcd))
     } else {
-        [(a + c) / gcd, (b + d) / gcd]
+        Some(((a + c) / gcd, (b + d) / gcd))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::decimal_to_rational;
+
+    #[test]
+    fn test_decimal_to_rational() {
+        assert_eq!(decimal_to_rational(0.5), Some((1, 2)));
+        assert_eq!(decimal_to_rational(1.0), Some((1, 1)));
+        assert_eq!(decimal_to_rational(f64::INFINITY), None);
+        assert_eq!(decimal_to_rational(f64::NAN), None);
     }
 }
